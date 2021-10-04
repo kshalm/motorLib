568734e2e1b4be218304dd32cd64c319712fdc73
kshalm <lynden.shalm@colorado.edu>
2021-10-04 14:53:28 -0600

    fixed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='bellMotors',
         version='0.2',
         description='Modularized bell_motors control.',
-        url='https://gitpi.bw.nist.gov/cks2/motors_module',
+        url='https://github.com/kshalm/motorLib',
         author='Krister Shalm',
         packages=['bellMotors'],
         )
