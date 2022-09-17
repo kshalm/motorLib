ab6f5172923db1527b4ed6f04acc630f7a8205e7
Gautam Kavuri <gautamadityakavuri@gmail.com>
2022-09-16 18:22:57 -0600

    changed to relative import py3

--- bellMotors/LinearController.py
+++ bellMotors/LinearController.py
@@ -6,27 +6,30 @@ Created on Fri Aug 07 17:13:32 2015
 """
 
 from .APTMotor import APTMotor
+
+
 class LinearController(APTMotor):
-    
+
     def __init__(self, info):
-        APTMotor.__init__(self,info['serial'], HWTYPE = 31)
-        APTMotor.setVelocityParameters(self, info['minVel'], info['acc'], info['maxVel'])
+        APTMotor.__init__(self, info['serial'], HWTYPE=31)
+        APTMotor.setVelocityParameters(
+            self, info['minVel'], info['acc'], info['maxVel'])
         APTMotor.getStageAxisInformation(self)
-        APTMotor.setStageAxisInformation(self, info['minPos'],info['maxPos'])
+        APTMotor.setStageAxisInformation(self, info['minPos'], info['maxPos'])
         self.attributes = info
-        
+
     def mAbs(self, absPosition):
         pos = absPosition + self.attributes['zero']
         if pos > self.attributes['maxPos'] or pos < self.attributes['minPos']:
             print("-------------------------------")
             print("ERROR: Outside range of motor")
             print("-------------------------------")
-            
+
             return "ERROR: Outside range of motor"
         else:
-            APTMotor.mAbs(self,pos)
+            APTMotor.mAbs(self, pos)
             return 'Success'
-            
+
     def mRel(self, step):
         pos = self.getPos() + step + self.attributes['zero']
         if pos > self.attributes['maxPos'] or pos < self.attributes['minPos']:
@@ -34,24 +37,24 @@ class LinearController(APTMotor):
             print("ERROR: Outside range of motor")
             print("-------------------------------")
             return "ERROR: Outside range of motor"
-            
+
         else:
-            #might be a problem with controller's mRel linear range check and zero offset
-            APTMotor.mRel(self,step)
+            # might be a problem with controller's mRel linear range check and zero offset
+            APTMotor.mRel(self, step)
             return "Success"
-            
+
     def getPos(self):
         absolutePos = APTMotor.getPos(self)
         return absolutePos - self.attributes['zero']
-    
-        
+
+
 if __name__ == '__main__':
-    motor1 ={
-    'name': 'motor1',
-    'serial': 83842776,
-    'minVel': 0.5,
-    'maxVel': 1.0,
-    'acc' : 1.0,
-    'zero': 0}
+    motor1 = {
+        'name': 'motor1',
+        'serial': 83842776,
+        'minVel': 0.5,
+        'maxVel': 1.0,
+        'acc': 1.0,
+        'zero': 0}
     motor = LinearController(motor1)
     print('created')
--- bellMotors/RotationController.py
+++ bellMotors/RotationController.py
@@ -5,24 +5,26 @@ Created on Wed Aug 05 12:10:18 2015
 @author: qittlab
 Collin Schlager
 """
-from APTMotor import APTMotor
-#from .APTMotor import APTMotor
+# from APTMotor import APTMotor
+from .APTMotor import APTMotor
+
 
 class RotationController(APTMotor):
 
     def __init__(self, info):
-        APTMotor.__init__(self,info['serial'], HWTYPE = 31)
-        APTMotor.setVelocityParameters(self,info['minVel'],info['acc'],info['maxVel'])
-        self.set_backlash_dist(1.00019) #sets the backlash correction
+        APTMotor.__init__(self, info['serial'], HWTYPE=31)
+        APTMotor.setVelocityParameters(
+            self, info['minVel'], info['acc'], info['maxVel'])
+        self.set_backlash_dist(1.00019)  # sets the backlash correction
         self.attributes = info
         self.linear_range = (0, 360)
 
     def mAbs(self, absPosition):
-        pos = (absPosition + self.attributes['zero'])%360
+        pos = (absPosition + self.attributes['zero']) % 360
         # print("Moving to %r (%r)..."%(absPosition,pos))
         try:
-            APTMotor.moveOptimal(self,pos)
-        except:
+            APTMotor.moveOptimal(self, pos)
+        except Exception:
             print('Failed')
             return 'Failed'
         # print('\t Moved to %r'%self.getPos())
@@ -45,36 +47,36 @@ class RotationController(APTMotor):
 
 
 if __name__ == '__main__':
-    motor1 ={
-    'name': 'motor1',
-    'serial': 83842776,
-    'minVel': 0.5,
-    'maxVel': 1.0,
-    'acc' : 1.0,
-    'zero': 0}
-    motor2 ={
-    'name': 'motor2',
-    'serial': 83854943,
-    'minVel': 0.5,
-    'maxVel': 1.0,
-    'acc' : 1.0,
-    'zero': 160.59}
-    BOBHWP2 ={
-    'acc': 25,
-    'maxVel': 25,
-    'minVel': 5,
-    'name': "BobHWP2",
-    'serial': 83854943,
-    'zero': 16.7,
-    'type': "rotational"}
+    motor1 = {
+        'name': 'motor1',
+        'serial': 83842776,
+        'minVel': 0.5,
+        'maxVel': 1.0,
+        'acc': 1.0,
+        'zero': 0}
+    motor2 = {
+        'name': 'motor2',
+        'serial': 83854943,
+        'minVel': 0.5,
+        'maxVel': 1.0,
+        'acc': 1.0,
+        'zero': 160.59}
+    BOBHWP2 = {
+        'acc': 25,
+        'maxVel': 25,
+        'minVel': 5,
+        'name': "BobHWP2",
+        'serial': 83854943,
+        'zero': 16.7,
+        'type': "rotational"}
     BOBHWP1 = {
-    'acc': 25,
-    'maxVel': 25,
-    'minVel': 5,
-    'name': 'BobHWP1',
-    'serial': 83857280,
-    'zero': -3.39,
-    'type': 'rotational'}
+        'acc': 25,
+        'maxVel': 25,
+        'minVel': 5,
+        'name': 'BobHWP1',
+        'serial': 83857280,
+        'zero': -3.39,
+        'type': 'rotational'}
 #
     motor = RotationController(BOBHWP1)
     print('Motor object created: ', motor)
